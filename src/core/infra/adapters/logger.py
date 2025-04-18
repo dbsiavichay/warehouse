0195a827aa5de0b44c6a649c73fb1e629813a1f7
Denis Braulio Siavichay Portilla <dbsiavichay@gmail.com>
2025-04-18 09:33:21 -0500

    feat: instrument open telemetry (#2)

---
+++ src/core/infra/adapters/logger.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+import structlog
+from src.core.domain.ports import Logger
+from structlog.processors import TimeStamper, add_log_level
+from structlog.stdlib import LoggerFactory, filter_by_level, render_to_log_kwargs
+
+
+class StructLogger(Logger):
+    _level: str = "NOSET"
+    _instance: Optional["StructLogger"] = None
+    _logger = None
+
+    def __new__(cls, level: str = "INFO") -> "StructLogger":
+        if cls._instance is None:
+            cls._instance = super(StructLogger, cls).__new__(cls)
+            cls._level = level
+            cls._instance.__initialize__()
+
+        return cls._instance
+
+    def __initialize__(self) -> None:
+        self._logger = structlog.get_logger()
+
+        structlog.configure(
+            processors=[
+                filter_by_level,
+                add_log_level,
+                TimeStamper(fmt="iso"),
+                render_to_log_kwargs,
+            ],
+            wrapper_class=structlog.make_filtering_bound_logger(self._level),
+            context_class=dict,
+            logger_factory=LoggerFactory(),
+            cache_logger_on_first_use=True,
+        )
+
+    def debug(self, message: str, **kwargs) -> None:
+        self._logger.debug(message, **kwargs)
+
+    def info(self, message: str, **kwargs) -> None:
+        self._logger.info(message, **kwargs)
+
+    def warning(self, message: str, **kwargs) -> None:
+        self._logger.warning(message, **kwargs)
+
+    def error(self, message: str, **kwargs) -> None:
+        self._logger.error(message, **kwargs)
+
+    def critical(self, message: str, **kwargs) -> None:
+        self._logger.critical(message, **kwargs)
+
+    def exception(self, message: str, **kwargs) -> None:
+        self._logger.exception(message, **kwargs)
